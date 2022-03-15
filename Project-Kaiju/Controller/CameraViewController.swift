704eaf2ce9e67e754a447f3c06d88ad2c722a550
goooochi <yuki.yamaguchi@ksf.biglobe.ne.jp>
2022-03-15 15:20:46 +0900

    3/15 ver2

--- Project-Kaiju/Controller/CameraViewController.swift
+++ Project-Kaiju/Controller/CameraViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class CameraViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate{
     
     @IBOutlet weak var imageView: UIImageView!
     
--- Project-Kaiju/Controller/ViewController.swift
+++ Project-Kaiju/Controller/ViewController.swift
@@ -9,13 +9,107 @@ import UIKit
 
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    
+    @IBOutlet weak var label1: UILabel!
+    
+    @IBOutlet weak var Label: UILabel!
+    
+    
+    
+    let upperPickerView = UIPickerView()
+    let upperData = [
+        "自動生成","アルバム","カメラ"
+    ]
+    
+    
+    
+//    let lowerPickerView = UIPickerView()
+//    let lowerData = [
+//
+//        "5","10","15"
+//
+//    ]
+    
+     
+    
+    @IBOutlet weak var selectPicker: UIPickerView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        selectPicker.delegate = self
+        selectPicker.dataSource = self
+        
+        
+        
     }
-
-
+    
+    
+    
+    
+    //IDつける
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        
+        return 1
+        
+    }
+    
+    // UIPickerViewの行数、リストの数
+        func pickerView(_ pickerView: UIPickerView,
+                        numberOfRowsInComponent component: Int) -> Int {
+            return upperData.count
+        }
+    
+    
+    // UIPickerViewの最初の表示
+       func pickerView(_ pickerView: UIPickerView,
+                       titleForRow row: Int,
+                       forComponent component: Int) -> String? {
+           
+           return upperData[row]
+       }
+            
+    
+    
+    // UIPickerViewのRowが選択された時の挙動
+        func pickerView(_ pickerView: UIPickerView,
+                        didSelectRow row: Int,
+                        inComponent component: Int) {
+            
+            Label.text = upperData[row]
+            
+        }
+    
+    
+    @IBAction func Next(_ sender: Any) {
+        
+        
+        if Label.text == upperData[0]{
+            
+            performSegue(withIdentifier: "API", sender: nil)
+        
+            
+        }
+        
+        if Label.text == upperData[1]{
+            
+            performSegue(withIdentifier: "Album", sender: nil)
+        
+            
+        }
+        
+        if Label.text == upperData[2]{
+            
+            performSegue(withIdentifier: "Camera", sender: nil)
+        
+            
+        }
+        
+        
+    }
+    
 }
-
